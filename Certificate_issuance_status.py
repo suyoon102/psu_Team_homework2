55255db20e6f25555e5591910f411f11a125a1e5
julia0519aa <julia0519aa@naver.com>
2021-05-18 13:24:17 +0900

    first upload : creating Certificate_issuance_status.py

--- Certificate_issuance_status.py
+++ Certificate_issuance_status.py
@@ -1,27 +1,70 @@
 from selenium import webdriver
+import matplotlib.pyplot as plt
+import matplotlib
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
-chromedriver = 'C:/Users/Win10Pro/PycharmProjects/chromedriver.exe' # chromedriver.exe 파일 위치에 따라
-driver = webdriver.Chrome(chromedriver, options=options)
+driver = webdriver.Chrome('chromedriver',options=options)
 driver.get('https://www.airportal.go.kr/knowledge/statsnew/employee/license.jsp')
 
-#print('항공종사자 자격증명 발급현황')
-#print(driver.current_url)
-#search_box = driver.find_elements_by_tag_name()
+mySheet_table = driver.find_element_by_xpath('//*[@id="mySheet-table"]/tbody/tr[2]/td/div/div[1]/table/tbody')
 
-mySheet_table = driver.find_element_by_xpath('//*[@id="mySheet-table"]')
-#print(mySheet_table)
-#print(mySheet_table.text, end=" [] ")
-for i in mySheet_table.text:
-    print(i, end=" ")
+cyc = 0
+lists = []
+for i in mySheet_table.find_elements_by_css_selector('tr'):
+    count = 0
+    cyc += 1
+    list = []
+    for m in i.find_elements_by_css_selector('td'):
+        count += 1
 
-# mySheet_table = driver.find_element_by_xpath('//*[@id="mySheet-table"]')
-# mySheet_table_tr_list = mySheet_table.find_elements_by_tag_name("tr")
-# for mySheet_table_tr in mySheet_table_tr_list:
-#     mySheet_table_tr_td_list = mySheet_table_tr.find_elements_by_tag_name("td")
-#     for mySheet_table_tr_td in mySheet_table_tr_td_list:
-#         print("\t", mySheet_table_tr_td.text, end="\tl-l\t")
-#     print("")
+        if count == 1:
+            continue
 
+        print(m.text,end=' ')
+        list.append(m.text)
+
+        if count == 5:
+            break
+
+    print('')
+    lists.append(list)
+
+    if cyc == 14:
+        break
+
+del lists[0]
 driver.close()
+
+year_list = []
+list1 = []
+list2 = []
+list3 = []
+
+for i in lists:
+    year_list.append(int(i[0]))
+    i[1] = i[1].replace(',','')
+    i[2] = i[2].replace(',', '')
+    i[3] = i[3].replace(',', '')
+    list1.append(int(i[1]))
+    list2.append(int(i[2]))
+    list3.append(int(i[3]))
+
+print('')
+print(year_list)
+print(list1)
+print(list2)
+print(list3)
+
+matplotlib.rcParams["axes.unicode_minus"]=False
+plt.rc('font', family='Malgun Gothic')
+
+plt.plot(range(1,14),list1,color='lightskyblue',linewidth=2.5,label='운송용')
+plt.plot(range(1,14),list2,color='lightpink',linewidth=2.5,label='사업용')
+plt.plot(range(1,14),list3,color='lightgreen',linewidth=2.5,label='자가용')
+plt.suptitle('항공종사자 자격증명 발급현황')
+plt.xticks(range(1,14),year_list,fontsize=9)
+plt.xlabel('년도',fontsize=9)
+plt.ylabel('(단위:명)',fontsize=9)
+plt.legend()
+plt.show()
